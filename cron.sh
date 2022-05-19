e01fde58f0d0f141d2a079aba6c60f459b8a9fce
Jake Mulford <jakemulf@gmail.com>
2022-05-19 14:39:09 -0400

    dtw lock workaround

--- cron.sh
+++ cron.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-##Example usage: DAYS_OFFSET=0 SCHEDULED=true . ./cron.sh python3
+##Example usage: . ./cron.sh python3
 sh start.sh -d
 $1 -m venv venv
 source venv/bin/activate
 pip install -r scripts/requirements.txt
-python scripts/run_scripts.py scripts/ga_main.py,scripts/twitter_main.py,scripts/slack_main.py,scripts/parquet_writer.py,scripts/scheduler.py localhost 10000 30
+python scripts/run_scripts.py scripts/ga_main.py,scripts/twitter_main.py,scripts/slack_main.py,scripts/parquet_writer.py,scripts/scheduler.py,scripts/writer.py localhost 10000 30
 docker-compose down
 deactivate
 rm -rf venv
---
+++ scripts/writer.py
@@ -0,0 +1,16 @@
+"""
+write.py
+An extension to the scheduler to write the tables to parquet files, this is a workaround
+for the DynamicTableWriter update lock.
+"""
+import os
+
+if not bool(os.environ.get("SCHEDULED", False)):
+    print("SCHEDULED needs to be set to \"true\" to run the scheduler. Skipping the scheduler...")
+else:
+    ###Write tables
+    write_tables(tables=ga_tables, path=f"/data/{start_date.toDateString()}/google/")
+    write_tables(table=twitter_analytics_table, path=f"/data/{start_date.toDateString()}/twitter/")
+    write_tables(table=twitter_metadata, path=f"/data/{start_date.toDateString()}/twitter-metadata/")
+    write_tables(table=slack_channels, path=f"/data/{start_date.toDateString()}/slack-channels/")
+    write_tables(table=slack_messages, path=f"/data/{start_date.toDateString()}/slack-messages/")
