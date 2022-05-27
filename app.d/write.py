88ff9094d78ac836ad10202445b3f063599a7bbd
Jake Mulford <jakemulf@gmail.com>
2022-05-27 14:08:08 -0400

    added env variables to enable/disable what data is collected

--- app.d/write.py
+++ app.d/write.py
@@ -10,8 +10,11 @@ if not bool(os.environ.get("SCHEDULED", False)):
     print("SCHEDULED needs to be set to \"true\" to run the scheduler. Skipping the scheduler...")
 else:
     ###Write tables
-    write_tables(tables=ga_tables, path=f"/data/{start_date.toDateString()}/google/")
-    write_tables(table=twitter_analytics_table, path=f"/data/{start_date.toDateString()}/twitter/")
-    write_tables(table=twitter_metadata, path=f"/data/{start_date.toDateString()}/twitter-metadata/")
-    write_tables(table=slack_channels, path=f"/data/{start_date.toDateString()}/slack-channels/")
-    write_tables(table=slack_messages, path=f"/data/{start_date.toDateString()}/slack-messages/")
+    if bool(os.environ.get("ENABLE_GA"), False):
+        write_tables(tables=ga_tables, path=f"/data/{start_date.toDateString()}/google/")
+    if bool(os.environ.get("ENABLE_TWITTER"), False):
+        write_tables(table=twitter_analytics_table, path=f"/data/{start_date.toDateString()}/twitter/")
+        write_tables(table=twitter_metadata, path=f"/data/{start_date.toDateString()}/twitter-metadata/")
+    if bool(os.environ.get("ENABLE_SLACK"), False):
+        write_tables(table=slack_channels, path=f"/data/{start_date.toDateString()}/slack-channels/")
+        write_tables(table=slack_messages, path=f"/data/{start_date.toDateString()}/slack-messages/")
